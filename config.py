e9c136f30cb42206fc028cc3744e4819453c2cce
Benjamin Pla <benjaminpla.dev@gmail.com>
2023-10-30 21:06:30 +0100

    update config.py

--- config.py
+++ config.py
@@ -78,17 +78,8 @@ screens = [
             ],
             25,
         ),
-        wallpaper='~/.config/qtile/wallaper.jpg',
-        wallpaper_mode='stretch',
     ),
 ]
 
-dgroups_key_binder = None
-dgroups_app_rules = []
 follow_mouse_focus = False
 bring_front_click = False
-floats_kept_above = True
-cursor_warp = False
-auto_fullscreen = True
-focus_on_window_activation = "smart"
-reconfigure_screens = True
